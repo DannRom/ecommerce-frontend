0c517e60d23d1dcba156bcf9343daedf436d52cb
Danny Romero <37481019+DannRom@users.noreply.github.com>
2021-09-07 11:32:25 -0500

    Update product.ts

--- src/app/model/product.ts
+++ src/app/model/product.ts
@@ -1,11 +1,9 @@
 export class Product {
   id!: string;
-  sku!: string;
   name!: string;
   description!: string;
   unitPrice!: number;
   imageUrl!: string;
-  active!: boolean;
   unitsInStock!: number;
   dataCreated!: Date;
   lastUpdate!: Date;
