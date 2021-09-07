97ab767fd95df97d7704edd41c3a981eeaea09da
Danny Romero <37481019+DannRom@users.noreply.github.com>
2021-09-07 13:14:23 -0500

    Update to entities

---
+++ src/app/components/checkout/checkout.entity.ts
@@ -0,0 +1,43 @@
+import { ProductInCart } from '../../model/product-in-cart';
+
+export interface Address {
+  street: string;
+  city: string;
+  state: string;
+  country: string;
+  zipCode: string;
+}
+
+export interface Customer {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
+export class Order {
+  totalQuantity!: number;
+  totalPrice!: number;
+}
+
+export class OrderItem {
+  productId: string;
+  quantity: number;
+  unitPrice: number;
+  imageUrl: string;
+
+
+  constructor(productInCart: ProductInCart) {
+    this.productId = productInCart.id;
+    this.quantity = productInCart.quantity;
+    this.unitPrice = productInCart.unitPrice;
+    this.imageUrl = productInCart.imageUrl;
+  }
+}
+
+export class Purchase {
+  customer!: Customer;
+  billingAddress!: Address;
+  shippingAddress!: Address;
+  order!: Order;
+  orderItems!: OrderItem[];
+}
